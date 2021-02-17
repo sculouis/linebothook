0ed44048dd61072c2df1d2b50e1993048f085dbe
sculouis <scu.louis@gmail.com>
2021-02-17 15:30:51 +0800

    import urlrequest

--- clock.py
+++ clock.py
@@ -1,8 +1,9 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+import urllib.request
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', minute='*/2')
+@sched.scheduled_job('cron', day_of_week='mon-fri', minute='*/1')
 def scheduled_job():
     url = "https://linebothook.herokuapp.com/"
     conn = urllib.request.urlopen(url)
