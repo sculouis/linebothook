f619c3e43d322c2bae6af1976d773dbce804d1cd
sculouis <scu.louis@gmail.com>
2021-02-17 16:25:25 +0800

    SET TRIGGER TO 8-20

--- clock.py
+++ clock.py
@@ -1,9 +1,11 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.combining import OrTrigger
+from apscheduler.triggers.cron import CronTrigger
 import urllib.request
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', minute='*/1')
+# @sched.scheduled_job('cron', day_of_week='mon-fri', minute='*/1')
 def scheduled_job():
     url = "https://linebothook.herokuapp.com/"
     conn = urllib.request.urlopen(url)
@@ -11,4 +13,9 @@ def scheduled_job():
     for key, value in conn.getheaders():
         print(key, value)
 
+trigger = OrTrigger([
+   CronTrigger(hour='10-20', minute='*/20')
+])
+
+sched.add_job(scheduled_job, trigger)
 sched.start()
